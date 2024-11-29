04eb5f403c6bd7e1933351f7ca4baa18d2922f15
Brayan Sanchez <brayan_sanchez99@hotmail.es>
2024-11-28 19:06:16 -0600

    Adding connection to mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 // const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 
 // Conectar a la base de datos
-// connectDB();
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((error) => console.log('Error conectando a MongoDB:', error));
 
 // Middleware para parsear el body
 // app.use(express.json({ extended: false }));
 
 // Rutes
 app.get('/', (req, res) => res.send('API Running'));
+// app.use('/api/pacientes', require('./routes/pacientes'));
+// app.use('/api/sintomas', require('./routes/sintomas'));
 
-// Define tus rutas aquí
-
-const PORT = process.env.PORT || 5000;
+// Server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
